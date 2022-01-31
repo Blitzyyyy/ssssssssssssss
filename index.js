ba5033d30042392c4c16b312a5a548e0d3df8a92
Blitzyyyy <90345135+Blitzyyyy@users.noreply.github.com>
2022-01-31 14:21:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+    host: 'ult9.falix.gg',
+    port: 39938,
+    version: "1.17.1",
+    username: 'Jamie',
+    //auth: "mojang"
+});
+
+bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    bot.chat(message)
+});
+
+bot.on('kicked', console.log)
+bot.on('error', console.log)
+
+bot.on("move", ()=>{
+    let friend = bot.nearestEntity();
+
+    if (friend) {
+        bot.lookAt(friend.position.offset(0, friend.height, 0));
+    }
+    
+});
